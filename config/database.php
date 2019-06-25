ef8714f0e1c433201855a6e7d2c3889e64213472
iankiragu <iankiragu63@gmail.com>
2019-06-25 15:02:24 +0300

    commit message

--- config/database.php
+++ config/database.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Str;
 $ url= parse_url(getenv("CLEARDB_DATABASE_URL"));
 $host= $url["host"];
-$username=$["user"];
-$password=$["pass"];
+$username=$url["user"];
+$password=$url["pass"];
 $database= substr($url["path"], 1);
 
 
